31c06f71db066321ed59764921369355e9820f23
DESKTOP-POFF5J0\Vital <vitaliytsn@gmail.com>
2018-05-27 16:44:58 +0200

    dodaem wiecej sumaryzatorow i kwalifikatorow

--- KSR2/KSR2/Program.cs
+++ KSR2/KSR2/Program.cs
@@ -34,7 +34,7 @@ namespace KSR2
             Fuzzy1 capitalLoses = new Fuzzy1(people, 11);
             Fuzzy1 hoursPerWeeks = new Fuzzy1(people, 12);
             #endregion
-         
+
             #region Labels
             //Black Races
             Label raceLabel = new Label("Black", new TriangleFunc(4, 1), races);
@@ -45,18 +45,28 @@ namespace KSR2
             //Hight Education
             Label hightEducation = new Label("with Hight Level Education ", new TriangleFunc(1, 2), educations);
 
-            //avarageAge With triangle Function
-            Label avarageAgeTriangle = new Label("Midle Age ", new TriangleFunc(30, 10), ages);
-
             //avarageAge With trapezoid Function
-            Label avarageAgeTrapezoid = new Label("Midle AgeTra", new TrapezoidFunc(25, 20, 35, 40), ages);
+            Label avarageAgeTrapezoid = new Label("Midle Age", new TrapezoidFunc(25, 20, 35, 40), ages);
 
             //Maried People
             Label merried = new Label("Maried ", new TriangleFunc(0, 1), martialStatuses);
 
+            //Self mployed people
+            Label selfEmployed = new Label("Self Employed", new TrapezoidFunc(0.5, 1.2, 1.8, 2.2), workClases);
+
+            //gender = male
+            Label males = new Label("Males", new TriangleFunc(1, 0.5), genders);
+
+            //occupation= managers
+            Label managers = new Label("Maneges", new TrapezoidFunc(2.5, 3.8, 4, 5.2), occupations);
+
             //Hight Education or avarage Salaries
             Label hightEducation_or_AvarageSalaries = new Label(hightEducation);
             hightEducation_or_AvarageSalaries.FuzzySubraction(avarageSalaries);
+
+            //managers who has avarage salary
+            Label managers_and_avarageSalaries = new Label(avarageSalaries);
+            managers_and_avarageSalaries.FuzzySumm(managers);
 
             //Hight Education or Black race
             Label hightEducation_or_Black = new Label(hightEducation);
@@ -71,7 +81,7 @@ namespace KSR2
             hightEducation_And_Married.FuzzySumm(hightEducation);
 
             //middle age and Avarage salaries
-            Label middleAge_and_AvarageSalaries = new Label(avarageAgeTriangle);
+            Label middleAge_and_AvarageSalaries = new Label(avarageAgeTrapezoid);
             middleAge_and_AvarageSalaries.FuzzySumm(avarageSalaries);
 
             //hight Education or Married
@@ -91,24 +101,30 @@ namespace KSR2
 
             #region Qualificators
             List<Qualificator> qualificators = new List<Qualificator>();
-            Qualificator raceQualificator = new Qualificator("which are Black race", new TriangleFunc(4, 1), avarageSalaries,races);
-            Qualificator hightEducation_Married = new Qualificator(" who has Hight Level Education ", new TriangleFunc(1, 2), merried,educations);
-            Qualificator avarageAgeTriangle_AvarageSalaries = new Qualificator(" has middle age ", new TriangleFunc(30, 10), hightEducation_Or_Married_Or_Black, ages);
+            Qualificator raceQualificator = new Qualificator("Which Are Black race", new TriangleFunc(4, 1), avarageSalaries, races);
+            Qualificator hightEducation_Married = new Qualificator(" Who Has Hight Level Education ", new TriangleFunc(1, 2), merried, educations);
+            Qualificator avarageAgeTriangle_AvarageSalaries = new Qualificator(" Has Middle Age ", new TriangleFunc(30, 10), hightEducation_Or_Married_Or_Black, ages);
+            Qualificator managers_Avarage_Salaries = new Qualificator("who are Maneges ", new TrapezoidFunc(2.5, 3.8, 4, 5.2),avarageSalaries ,occupations);
             qualificators.Add(avarageAgeTriangle_AvarageSalaries);
             qualificators.Add(raceQualificator);
             qualificators.Add(hightEducation_Married);
+            qualificators.Add(managers_Avarage_Salaries);
             Linquistic lq = new Linquistic(qualificators, quantyficators);
             lq.generateOutput();
             #endregion
 
             #region Labels
-                lableList.Add(raceLabel);
-                lableList.Add(avarageSalaries);
-                 lableList.Add(hightEducation);
-                lableList.Add(avarageAgeTriangle);
-                lableList.Add(merried);
-                lableList.Add(avarageAgeTrapezoid);
-           
+            lableList.Add(males);
+            lableList.Add(managers);
+            lableList.Add(selfEmployed);
+            lableList.Add(raceLabel);
+            lableList.Add(avarageSalaries);
+            lableList.Add(hightEducation);
+            lableList.Add(avarageAgeTrapezoid);
+            lableList.Add(merried);
+            lableList.Add(avarageAgeTrapezoid);
+
+            lableList.Add(managers_and_avarageSalaries);
             lableList.Add(middleAge_and_AvarageSalaries);
             lableList.Add(blackRace_and_AvarageSalaries);
             lableList.Add(hightEducation_And_Married);
